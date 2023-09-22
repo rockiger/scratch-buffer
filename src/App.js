a2172b357eb2ae8317639c8a559d2e557372a26f
rockiger <marco@rockiger.com>
2023-09-22 13:09:43 +0200

    First version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Highlight from '@tiptap/extension-highlight'
+import Typography from '@tiptap/extension-typography'
+import { EditorContent, useEditor } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
+import React from 'react'
+import { debounce } from 'lodash'
+
+const localStorageKey = 'com.rockiger.scratch-buffer'
+
+// eslint-disable-next-line
+export default () => {
+  const editor = useEditor({
+    autofocus: true,
+    onUpdate: debounce(() => {
+      localStorage.setItem(localStorageKey, editor.getHTML())
+    }, 1000),
+    extensions: [StarterKit, Highlight, Typography],
+    editorProps: {
+      attributes: {
+        class: '',
+      },
+    },
+    content: getContent(),
+  })
+
+  return <EditorContent editor={editor} />
 }
 
-export default App;
+function getContent() {
+  const content = localStorage.getItem(localStorageKey)
+  if (content == null) return getDefaultContent()
+  return content
+}
+
+function getDefaultContent() {
+  return `
+    <p>
+      Markdown shortcuts make it easy to format the text while typing.
+    </p>
+    <p>
+      To test that, start a new line and type <code>#</code> followed by a space to get a heading. Try <code>#</code>, <code>##</code>, <code>###</code>, <code>####</code>, <code>#####</code>, <code>######</code> for different levels.
+    </p>
+    <p>
+      Those conventions are called input rules in tiptap. Some of them are enabled by default. Try <code>></code> for blockquotes, <code>*</code>, <code>-</code> or <code>+</code> for bullet lists, or <code>\`foobar\`</code> to highlight code, <code>~~tildes~~</code> to strike text, or <code>==equal signs==</code> to highlight text.
+    </p>
+    <p>
+      You can overwrite existing input rules or add your own to nodes, marks and extensions.
+    </p>
+    <p>
+      For example, we added the <code>Typography</code> extension here. Try typing <code>(c)</code> to see how it’s converted to a proper © character. You can also try <code>-></code>, <code>>></code>, <code>1/2</code>, <code>!=</code>, or <code>--</code>.
+    </p>
+    `
+}
